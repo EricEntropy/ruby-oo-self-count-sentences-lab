23253e79ac8d5a85e9f1753ded0c9ab67306026a
Eric <contreras.eric18@gmail.com>
2021-01-30 16:20:19 -0500

    commit tree a1b35f90da9aed75da8a9da4d127bb61de918dec

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,30 @@ require 'pry'
 
 class String
 
+  def endchecker(check)
+    if self.end_with?(check)
+      return true
+    else 
+      false 
+    end
+  end 
+
   def sentence?
-    
+    endchecker(".")
   end
 
   def question?
-
+    endchecker("?")
   end
 
   def exclamation?
-
+    endchecker("!")
   end
 
   def count_sentences
-
+    array = []
+    #greedy repetition from "+" = repeated one or more times 
+    array = self.split(/[.?!]+/)
+    return array.length
   end
 end
